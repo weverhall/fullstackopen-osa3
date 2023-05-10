70e56b18bffc06cef3f1334d6ea5d098e922233e
w <weverhall@gmail.com>
2023-05-10 15:19:56 +0300

    3.7 & 3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
+
+morgan.token('postData', (request) => {
+  return JSON.stringify(request.body)
+})
 
 let persons = [
   { 
