c2a45ed176018a05d1f9583f3e17d42d8fe8a01c
w <weverhall@gmail.com>
2023-05-11 23:42:24 +0300

    update build

--- index.js
+++ index.js
@@ -2,42 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const app = express()
 const Person = require('./models/person')
 
-const app = express()
-
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
-app.use(cors())
 
 morgan.token('postData', (request) => {
   return JSON.stringify(request.body)
 })
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 app.get('/api/persons', (request, response) => {
   Person.find({})
